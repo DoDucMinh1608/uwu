e5c2b05d2389466a17b413401dea5bcae2fa0a0d
___ ____ __ <ducminhdo08@gmail.com>
2022-09-15 08:59:56 +0700

    uwu

--- src/controllers/index.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-  const router = Router()
-
-  router.route('/').get((req, res) => {
-    res.render('pages/index/index')
-  })
-  export default router
--- src/createPath.js
+++ src/createPath.js
@@ -9,12 +9,12 @@ function addFile(path, data = '') {
   fs.writeFileSync(path, data, { encoding: 'utf-8' })
 }
 const controllerData = dir => `import { Router } from 'express'
-  const router = Router()
+const router = Router()
 
-  router.route('/').get((req, res) => {
-    res.render('pages/${dir}/index')
-  })
-  export default router`
+router.route('/').get((req, res) => {
+  res.render('pages/${dir}/index')
+})
+export default router`
 const scssData = `body {
   width: 100vw;
   height: 100vh;
@@ -31,13 +31,58 @@ ${dir.toUpperCase()}
 
 <%- contentFor('body') %>
 <h1>${dir}</h1>`
+
+const layout = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><%- defineContent('title') %></title>
+
+  <link rel="stylesheet" href=".././../css/reset.css">
+  <script src="/socket.io/socket.io.js" defer></script>
+  <script src="https://ajax.aspnetcdn.com/ajax/jquery/jquery-1.9.0.min.js" defer type="module"></script>
+
+  <%- defineContent('init') %>
+</head>
+<body>
+  <%- body %>
+</body>
+</html>
+`
+const indexJSData = dir => `const socket = io()
+console.log('${dir.toUpperCase()}')`
 function createDir(dir, file, data) {
   if (!fs.existsSync(dir)) fs.mkdirSync(dir);
   addFile(join(dir, file), data)
 }
+function checkFolder(path) {
+  const dir = join(path);
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir)
+}
+
+function initProject() {
+  checkFolder(join(__dirname, 'controllers'))
+
+  checkFolder(join(__dirname, 'scss'))
+  checkFolder(join(__dirname, 'scss', 'stylesheets'))
+  addFile(join(__dirname, 'scss', 'reset.scss'), `@import "../../node_modules/scss-reset/reset";`)
+
+  checkFolder(join(__dirname, 'public'))
+  checkFolder(join(__dirname, 'public', 'js'))
+  checkFolder(join(__dirname, 'public', 'imgs'))
+
+  checkFolder(join(__dirname, 'views'))
+  checkFolder(join(__dirname, 'views', 'pages'))
+  checkFolder(join(__dirname, 'views', 'layouts'))
+  checkFolder(join(__dirname, 'views', 'partials'))
+  addFile(join(__dirname, 'views', 'layouts', 'layout.ejs'), layout)
+}
 function createFile(dir) {
   if (!dir) return
-  const dirs = fs.readdirSync(__dirname)
+  initProject()
+  let dirs = fs.readdirSync(__dirname)
   for (const link of dirs) {
     const file = join(__dirname, link)
     if (!(fs.lstatSync(file)).isDirectory()) continue
@@ -46,8 +91,7 @@ function createFile(dir) {
       continue
     }
     if (link == 'public') {
-      createDir(join(file, 'js', dir), 'index.js', '')
-      fs.mkdirSync(join(file, 'imgs', dir))
+      createDir(join(file, 'js', dir), 'index.js', indexJSData(dir))
       continue
     }
     if (link == 'scss') {
@@ -55,9 +99,10 @@ function createFile(dir) {
       continue
     }
     if (link == 'views') {
+
       createDir(join(file, 'pages', dir), 'index.ejs', ejsData(dir))
       continue
     }
   }
 }
-createFile('uwuwuw')
+createFile('index')
