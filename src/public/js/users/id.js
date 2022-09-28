38a447a1f5580f38150aec9716a0e8c0241ff24b
_____ <77343519+DoDucMinh1608@users.noreply.github.com>
2022-09-28 13:42:41 +0700

    e

---
+++ src/public/js/users/id.js
@@ -0,0 +1,19 @@
+const account = JSON.parse(localStorage.getItem('account'))
+if (!account) location.replace('/account2/login')
+if (account.id && !document.URL.includes(account.id)) location.replace(`./${account.id}`)
+
+const xhttp = new XMLHttpRequest()
+xhttp.open("POST", "./validate", true);
+xhttp.setRequestHeader('Content-type', 'application/json;charset=UTF-8')
+xhttp.onload = function () {
+  const data = JSON.parse(this.responseText)?.verify
+  if (data) return
+  localStorage.removeItem('account')
+  location.replace('/account2/login')
+};
+
+xhttp.send(JSON.stringify(account));
+
+$('.Change').click(e => {
+  location.replace('./change')
+})
